6ea9b91f78f436b17765d88ee8b11372981677f2
Ruiz Sanchez, Manuel <manuel.ruiz.sanchez@melia.com>
2023-11-07 13:48:00 +0100

    [wiremock] add openApi to config swagger

---
+++ wiremock-proxy-services-web/src/main/java/com/mfruizs/service/wiremock/configuration/SwaggerOpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.mfruizs.service.wiremock.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SwaggerOpenApiConfig {
+	@Bean
+	public OpenAPI baseOpenAPI(){
+		return new OpenAPI().components(new Components())
+			.info(new Info()
+					  .title("Springboot_Swagger Project OpenAPI Docs")
+					  .version("1.0.0").description("Doc Description"));
+	}
+}
